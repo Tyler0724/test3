4565bbd9e38795b7beab55f7de0ee73cdf81a9b5
Tyler0724 <63685714+Tyler0724@users.noreply.github.com>
2022-08-28 19:21:27 -0600

    Add files via upload

---
+++ exercise1.cpp
@@ -0,0 +1,15 @@
+// Exercise 1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World from smittyl4@isu.edu" << endl;
+}
+
+//Tyler
+//Smith
+//Spring2022
+//smittyl4@isu.edu
